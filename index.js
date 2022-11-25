bff9e7c1be91d5c0e997941c2d1772d26a35557d
Tareq35 <a.t.tariqulislam@gmail.com>
2022-11-26 02:46:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res)=> {
+    res.send('LapSell Corner server is running');
+});
+
+app.listen(port, () => console.log(`LapSell Corner running on ${port}`));
