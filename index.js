b5efd7f6e3b8be30523291cff58559d6179060db
Tareq35 <a.t.tariqulislam@gmail.com>
2022-11-29 01:24:10 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req, res)=> {
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x8ynutp.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', async (req, res) => {
     res.send('LapSell Corner server is running');
 });
 
